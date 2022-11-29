55e28f0f00bc8abffa96ddac7ca54f2e14481c85
Kyle Michael Deguzman <kylemichaeldeguzman@Kyles-MBP.attlocal.net>
2022-11-29 09:29:55 -0800

    edited app.js file

--- app.js
+++ app.js
@@ -1,101 +1,56 @@
-document.getElementById('btn-country1').addEventListener('click', data1);
-document.getElementById('btn-country2').addEventListener('click', data2);
-document.getElementById('btn-country3').addEventListener('click', data3);
-document.getElementById('btn-country4').addEventListener('click', data4);
-document.getElementById('btn-country5').addEventListener('click', data5);
+document.getElementById('btn-country1').addEventListener('click', loadData1);
+document.getElementById('btn-country2').addEventListener('click', loadData2);
 
-function data1() {
-    country1.innerHTML = `
-    <img src="./img/USA-flag.png" alt="USA">
-    <p>Country: United States of America</p>
-    <p>Continent: North America</p>
-    <p>Population: 331.9 million</p>
-    <p>President: Joe Biden</p> 
-    <p>Currency: US Dollar</p>
-    `;
+function loadData1(e) {
+    fetch('countryData.json')
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
 
-    const firstCountry = document.getElementById("country1");
-    const flagOne = document.getElementById("USA-flag");
+            let output = '';
 
-    if (firstCountry.style.display === "block") {
-      firstCountry.style.display = "none";
-    } else {
-      firstCountry.style.display = "block";
-    }
+            data.forEach((country) => {
+                output += `
+                    <li>Country: ${country.country1}</li>
+                    <br>
+                    <li>Population: ${country.population1}</li>
+                    <br>
+                    <li>President/Prime Minister: ${country.leader1}</li>
+                    <br>
+                    <li>Currency: ${country.currency1}</li>
+                `;
+
+                document.getElementById("output").innerHTML = output;
+            });
+        })
+        .catch(err => console.log(err));
+    
+    e.preventDefault();
 }
 
-function data2() {
-    country2.innerHTML = `
-    <img src="./img/Mexico-flag.png" alt="Mexico">
-    <p>Country: Mexico<p>
-    <p>Continent: North America</p>
-    <p>Population: 130.1 million<p>
-    <p>President: Andrés Manuel Lopez Obrador<p>
-    <p>Currency: Mexican Pesos<p>
-    `;
+function loadData2(e) {
+    fetch('countryData.json')
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
 
-    const secondCountry = document.getElementById("country2");
+            let output = '';
 
-    if (secondCountry.style.display === "block") {
-        secondCountry.style.display = "none";
-    } else {
-        secondCountry.style.display = "block";
-    }
-}
+            data.forEach((country) => {
+                output += `
+                    <li>Country: ${country.country2}</li>
+                    <br>
+                    <li>Population: ${country.population2}</li>
+                    <br>
+                    <li>President/Prime Minister: ${country.leader2}</li>
+                    <br>
+                    <li>Currency: ${country.currency2}</li>
+                `;
 
-function data3() {
-    country3.innerHTML = `
-    <img src="./img/Indonesian-flag.png" alt="Indonesia">
-    <p>Country: Indonesia</p>
-    <p>Continent: Asia</p>
-    <p>Population: 276.4 million</p>
-    <p>President: Joko Widodo</p>
-    <p>Currency: Indonesian Rupiah</p>
-    `;
-
-    const thirdCountry = document.getElementById("country3");
-
-    if (thirdCountry.style.display === "block") {
-        thirdCountry.style.display = "none";
-    } else {
-        thirdCountry.style.display = "block";
-    }
-}
-
-function data4() {
-    country4.innerHTML = `
-    <img src="img/Italian-flag.png" alt="Italy">
-    <p>Country: Italy</p>
-    <p>Continent: Europe</p>
-    <p>Population: 59.07 million</p>
-    <p>President: Sergio Mattarella</p>
-    <p>Currency: Euros</p>
-    `;
-
-    const fourthCountry = document.getElementById("country4");
-
-    if(fourthCountry.style.display === "block") {
-        fourthCountry.style.display = "none";
-    } else {
-        fourthCountry.style.display = "block";
-    }
-}
-
-function data5() {
-    country5.innerHTML = `
-    <img src="./img/Kenya-flag.png" alt="Kenya">
-    <p>Country: Kenya</p>
-    <p>Continent: Africa</p>
-    <p>Population: 54.99 million</p>
-    <p>President: William Ruto</p>
-    <p>Currency: Kenyan Shilling</p>
-    `;
-
-    const fifthCountry = document.getElementById("country5");
-
-    if(fifthCountry.style.display === "block") {
-        fifthCountry.style.display = "none";
-    } else {
-        fifthCountry.style.display = "block";
-    }
+                document.getElementById('output').innerHTML = output;
+            });
+        })
+        .catch(err => console.log(err));
+    
+    e.preventDefault();
 }
